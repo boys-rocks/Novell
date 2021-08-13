0444114555d06086689c5f74de49de28544fc660
baneetparmar <baneetparmar@pm.me>
2021-08-13 17:38:32 +0530

    optimitized to use heroku config-vars

--- bot.py
+++ bot.py
@@ -2,26 +2,27 @@ import discord
 from discord.ext import commands
 import os
 
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN", None)
 
-bot = commands.Bot(command_prefix=',', help_command=None)
+bot = commands.Bot(command_prefix=",", help_command=None)
 
 
 @bot.event
 async def on_ready():
-    print('Ready..')
-    print('Logged in as: ', bot.user)
-    print('Prefix: ', bot.command_prefix)
-    print('Latency: ', bot.latency)
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
+    print("Ready..")
+    print("Logged in as: ", bot.user)
+    print("Prefix: ", bot.command_prefix)
+    print("Latency: ", bot.latency)
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
         else:
-            print(f'Unable to load {filename}')
+            print(f"Unable to load {filename}")
 
 
 @bot.command()
 async def help(ctx):
-    await ctx.send('there is supposed to be a help command here.')
+    await ctx.send("there is supposed to be a help command here.")
 
 
-bot.run('ODc1NTcxODQ3Njc1MTUwMzc2.YRXd0w.R2brla6u5MjfCvuL6whMXxT9gXY')
+bot.run(DISCORD_TOKEN)
