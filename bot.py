0be156697079a1f330640783a669a17962cfccba
user <centipedemonster@gmail.com>
2021-08-13 17:58:50 +0530

    minor changes to bot.py line 14 to make latency readable

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def on_ready():
     print('Ready..')
     print('Logged in as: ', bot.user)
     print('Prefix: ', bot.command_prefix)
-    print('Latency: ', bot.latency)
+    print('Latency: ', round(bot.latency*1000), 'ms')
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
