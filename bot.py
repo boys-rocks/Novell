f7a2bc0226f8d932d5327d5940e5d848f202ce8f
user <centipedemonster@gmail.com>
2021-08-13 20:13:10 +0530

    added better help command

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def on_ready():
     print('Ready..')
     print('Logged in as: ', bot.user)
     print('Prefix: ', bot.command_prefix)
-    print('Latency: ', round(bot.latency*1000), 'ms')
+    print('Latency: ', round(bot.latency * 1000), 'ms')
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
@@ -29,45 +29,45 @@ async def ping(ctx):
 
 @bot.command()
 async def help(ctx):
-	docstring_values = await __parse_docstrings()
-	caller_message = ctx.message.content
-	if len(caller_message.split()) == 1:
-		# The message which called the help command has no params
-		message_text = "```\nAvailable Commands:\n"
-		for cog in docstring_values.keys():
-			message_text += cog
-		message_text += "\n```"
-		await ctx.send(message)
-	else:
-		# The command has parameters, search for cog with required name
-		cog = caller_message.split()[1]
-		try:
-			em = discord.Embed(title="Help", description="Get to know the commands")
-			for key, value in docstring_values[cog].items():
-				em.add_field(name=key, value=value, inline=False)
-			await ctx.send(embed=em)
-		except:
-			em = discord.Embed(title='Error', description=f'Could not find command {cog}')
-			await ctx.send(embed=em)
+    docstring_values = await __parse_docstrings()
+    caller_message = ctx.message.content
+    if len(caller_message.split()) == 1:
+        # The message which called the help command has no params
+        message_text = "```\nAvailable Commands:\n"
+        for cog in docstring_values.keys():
+            message_text += cog
+        message_text += "\n```"
+        await ctx.send(message_text)
+    else:
+        # The command has parameters, search for cog with required name
+        cog = caller_message.split()[1]
+        try:
+            em = discord.Embed(title="Help", description="Get to know the commands")
+            for key, value in docstring_values[cog].items():
+                em.add_field(name=key, value=value, inline=False)
+            await ctx.send(embed=em)
+        except:
+            em = discord.Embed(title='Error', description=f'Could not find command {cog}')
+            await ctx.send(embed=em)
 
 
 async def __parse_docstring(filename):
-	with open(filename, "r") as f:
-		contents = f.read()
-	module = ast.parse(contents)
-	docstring = ast.get_docstring(module)
-	if not docstring:
-		docstring = "description: <Unknown>\n" + \
-					"sintax: <Unknown>"
-	return {line.split(": "}[0]: "".join(line.split(": ")[1:]) for line in docstring.split("\n") if line.strip()}
+    with open(filename, "r") as f:
+        contents = f.read()
+    module = ast.parse(contents)
+    docstring = ast.get_docstring(module)
+    if not docstring:
+        docstring = "description: <Unknown>\n" + \
+                    "syntax: <Unknown>"
+    return {line.split(": ")[0]: "".join(line.split(": ")[1:]) for line in docstring.split("\n") if line.strip()}
 
-	
+
 async def __parse_docstrings():
-	values = {}
+    values = {}
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
-			values[filename] = await __parse_docstring(os.path.join("cogs", filename))
-	return values
+            values[filename] = await __parse_docstring(os.path.join("cogs", filename))
+    return values
 
 
 bot.run(DISCORD_TOKEN)
