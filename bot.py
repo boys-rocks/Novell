e9b34aa8f57f6333235a85b9eb2a98fb20ffdd6c
user <centipedemonster@gmail.com>
2021-08-13 18:06:19 +0530

    added ping command

--- bot.py
+++ bot.py
@@ -20,6 +20,12 @@ async def on_ready():
 
 
 @bot.command()
+async def ping(ctx):
+    em = discord.Embed(title='Pong!', description=f'{round(bot.latency * 100)} ms')
+    await ctx.send(embed=em)
+
+
+@bot.command()
 async def help(ctx):
     await ctx.send('there is supposed to be a help command here.')
 
