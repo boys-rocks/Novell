71a3c77fa2f8156ca13882ff84338ad5c9b4e0c4
Siddhant Kumar <79756171+centipede000@users.noreply.github.com>
2021-08-14 18:46:23 +0530

    added moderation cog

---
+++ cogs/Moderation.py
@@ -0,0 +1,8 @@
+import discord; from discord.ext import commands
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+def setup(bot):
+    bot.add_cog(Moderation(bot))
