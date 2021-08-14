2617848b9063b27d66c2fba8d3b08814585b620d
Thomas Staats <thomas.staats443@gmail.com>
2021-08-14 14:37:24 -0600

    Added papertrading cog
    Only one command currently:
    ,price symbol

---
+++ cogs/papertrading.py
@@ -0,0 +1,22 @@
+import sys
+import discord
+from discord.ext import commands 
+# setting path
+sys.path.append('../')
+from helpers import getPrice
+
+class Crypto(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def price(self,ctx,symbol):
+        currentPrice = getPrice.getPrice(symbol)
+        if currentPrice:
+            await ctx.send(f"The Current Price of {symbol} is {currentPrice}")
+        else:
+            await ctx.send("I'm having trouble finding that cryptocurrency, ,check for typos and try again :)")
+    
+def setup(bot):
+    bot.add_cog(Crypto(bot))
+    
