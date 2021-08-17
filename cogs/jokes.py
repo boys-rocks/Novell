980057369335f635a264ee2b357709ef815fafaa
user <centipedemonster@gmail.com>
2021-08-17 17:39:26 +0530

    added jokes command

---
+++ cogs/jokes.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import requests
+
+
+class Jokes(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def joke(self, ctx):
+        try:
+            x = requests.get('https://official-joke-api.appspot.com/random_joke')
+            await ctx.send(x.json()['setup'])
+            await ctx.send(x.json()['punchline'])
+        except Exception as ex:
+            print('Exception', ex)
+
+def setup(bot):
+    bot.add_cog(Jokes(bot))
