26e50b6ea769ba3bb79fd009c442f27d75565727
user <centipedemonster@gmail.com>
2021-08-17 18:16:20 +0530

    added affirm command

--- cogs/jokes.py
+++ cogs/jokes.py
@@ -16,5 +16,13 @@ class Jokes(commands.Cog):
         except Exception as ex:
             print('Exception', ex)
 
+    @commands.command()
+    async def affirm(self, ctx):
+        try:
+            x = requests.get('https://www.affirmations.dev/')
+            await ctx.send(x.json()['affirmation'])
+        except Exception as ex:
+            print('Exception: ', ex)
+
 def setup(bot):
     bot.add_cog(Jokes(bot))
