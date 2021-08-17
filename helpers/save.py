ef9e94e993d0f4d4c6da2c4614207fc0894b675b
Thomas Staats <thomas.staats443@gmail.com>
2021-08-16 20:00:57 -0600

    Papertrading helpers

---
+++ helpers/save.py
@@ -0,0 +1,20 @@
+import pickle as cPickle
+from helpers.logHelper import logger
+import classes.paper_trade_class
+def save(e,name):
+    try:
+        with open("userdata/"+name+".pickle", "wb") as output_file:
+            cPickle.dump(e, output_file)
+            print("Saved!")
+    except Exception as e:
+        logger.warning(e) 
+# pickle_file will be closed at this point, preventing your from accessing it any further
+def load(name):
+    try:
+        with open("userdata/"+name+".pickle", "rb") as input_file:
+            e = cPickle.load(input_file)
+            print(e)
+            print("Loaded!")
+            return e
+    except Exception as e:
+        logger.warning(e)
