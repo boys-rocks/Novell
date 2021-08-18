2c07cb8fde88c9b494419dae224fdae04c191b3b
user <centipedemonster@gmail.com>
2021-08-18 13:04:56 +0530

    activity command

---
+++ cogs/fun.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import requests
+
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(help='Get an activity')
+    async def activity(self, ctx):
+        import requests
+        x = requests.get("https://www.boredapi.com/api/activity")
+        await ctx.send(embed=discord.Embed(title=x.json()['activity'], description=x.json()['type']))
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
+
