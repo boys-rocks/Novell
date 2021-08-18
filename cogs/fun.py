f9f684f0bae7f729a6360ec3aa32b044118a6dde
Siddhant Kumar <79756171+centipede000@users.noreply.github.com>
2021-08-18 20:20:26 +0530

    fixed more syntax error

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,10 +10,10 @@ class Fun(commands.Cog):
     async def activity(self, ctx):
         import requests
         x = requests.get("https://www.boredapi.com/api/activity")
-        await ctx.send(embed=discord.Embed(title=x.json()['activity'], description=x.json()['type'])
+        await ctx.send(embed=discord.Embed(title=x.json()['activity'], description=x.json()['type']))
 
     @commands.command(help='Predict which country a name is from')
-    async def name(self, ctx, *, name):
+    async def nameinfo(self, ctx, *, name):
         y = requests.get(f'https://api.nationalize.io?name={name}')
         await ctx.send(f'Your name is likely from {y.json()['country'][0]['country_id']}')
 
