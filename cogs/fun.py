d429857b9e28075429f8ffa9675a7e0e41a23704
user <centipedemonster@gmail.com>
2021-08-19 15:03:12 +0530

    added country command

--- cogs/fun.py
+++ cogs/fun.py
@@ -20,6 +20,27 @@ class Fun(commands.Cog):
         result = y.json()['country'][0]['country_id']
         await ctx.send(f'Your name is likely from {result}')
 
+    @commands.command(help='Get information on a country')
+    async def country(self, ctx, *, name):
+        x = requests.get(f'https://restcountries.eu/rest/v2/name/{name}?fullText=true')
+        y = x.json()[0]
+        em = discord.Embed(title=name)
+        code1 = y['alpha2Code']
+        code2 = y['alpha3Code']
+        em.add_field(name='Country Code:', value=f'{code1}, {code2}')
+        callcode = y['callingCodes'][0]
+        em.add_field(name='Call Code:', value=f'{callcode}')
+        capital = y['capital']
+        em.add_field(name='Capital:', value=f'{capital}')
+        region1 = y['subregion']
+        region2 = y['region']
+        em.add_field(name='Regions:', value=f'{region1}, {region2}')
+        currency = y['currencies'][0]['code']
+        currency2 = y['currencies'][0]['name']
+        em.add_field(name='Currency:', value=f'{currency1}, {currency2}')
+        language = y['languages'][0]['name']
+        em.add_field(name='Languages:', value=f'{language}')
+        await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
