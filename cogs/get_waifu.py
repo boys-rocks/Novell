06968847697f26c1851cc56b9cf9df13e9e4fc4b
Baneet Parmar <ign.itami@gmail.com>
2021-08-21 22:59:37 +0530

    added black-action for autofomatting on commit

--- cogs/get_waifu.py
+++ cogs/get_waifu.py
@@ -3,16 +3,37 @@ from discord.ext import commands
 import requests
 
 
+
+
+
 class GetWaifu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+        
+        
+        
+        
 
     @commands.command()
     async def waifu(self, ctx, query):
         with requests.get(url=f"https://api.waifu.pics/sfw/{query}") as response:
             try:
+                
+                
+                
+                
                 await ctx.send(response.json()["url"])
+                
+                
+                
+                
+                
             except:
+                
+                
+                
+                
                 await ctx.send("Looks like no one likes you")
 
 
