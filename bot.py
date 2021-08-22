ea2a91e58f4f76f7b4c910c16f8ad334850f88ea
Thomas Staats <thomas.staats443@gmail.com>
2021-08-22 01:49:33 -0600

    used black to format

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ import ast
 from helpers.getWeather import getLocationKey, getWeather
 import time
 from pretty_help import PrettyHelp
+
 logging.basicConfig(level=logging.INFO)
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN", None)
@@ -16,21 +17,22 @@ MONGODB = os.environ.get("MONGODB", None)
 
 
 bot = commands.Bot(command_prefix="*", help_command=PrettyHelp())
-#bot = commands.Bot(command_prefix='*', help_command=None)
+# bot = commands.Bot(command_prefix='*', help_command=None)
 
 client = MongoClient(MONGODB)
 db = client["discord"]
 collection = db["bot"]
 
-for filename in os.listdir('./cogs'):
+for filename in os.listdir("./cogs"):
     try:
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
             logger.info(f"Succesfully Loaded Cog: {filename}")
         else:
             print(f"Unable to load {filename}")
             logger.warning(
-                f"Unable to load {filename}, is it suppose to be in cog directory?")
+                f"Unable to load {filename}, is it suppose to be in cog directory?"
+            )
     except Exception as e:
         logger.warning(f"Unable to load cog: {e}")
 """
@@ -40,10 +42,12 @@ if not collection.find_one({"_id": "word_command_freq"}):
     freq_data_exist = collection.find_one({"_id": "word_command_freq"})
     print(f"\n\n\n\nfreq data exist or na{freq_data_exist}\n\n\n")
     collection.insert_one({"_id": "word_command_freq"})
+
+
 @bot.event
 async def on_message(message):
     user = message.author
-    contents = message.content.split(' ')
+    contents = message.content.split(" ")
     word = contents[0]
     member = str(message.author)
     if user.bot:
@@ -57,29 +61,26 @@ async def on_message(message):
             if "$" in word:
                 word = word.replace("$", "(Dollar_Sign)")
         except Exception as e:
-            print(str(e)+"Caught in on_message")
+            print(str(e) + "Caught in on_message")
             logger.warning(e)
-        print(member+": "+word)
-        #is_in_word_command_freq=collection.find_one({"_id":"word_command_freq",word:{"$size": 0}})
+        print(member + ": " + word)
+        # is_in_word_command_freq=collection.find_one({"_id":"word_command_freq",word:{"$size": 0}})
         # print(is_in_word_command_freq)
         if collection.find_one({"_id": "word_command_freq", word: {"$exists": True}}):
-            collection.update_one(
-                {"_id": "word_command_freq"}, {"$inc": {word: 1}})
-            print("incremented freq value " + word +
-                  " by 1 in word_command_freq doc")
+            collection.update_one({"_id": "word_command_freq"}, {"$inc": {word: 1}})
+            print("incremented freq value " + word + " by 1 in word_command_freq doc")
         else:
-            print(collection.update(
-                {"_id": "word_command_freq"}, {"$set": {word: 1}}))
+            print(collection.update({"_id": "word_command_freq"}, {"$set": {word: 1}}))
             print("added " + word + " to word_command_freq")
-        #print(collection.find_one({"_id": "word_command_freq"}))
+        # print(collection.find_one({"_id": "word_command_freq"}))
     await bot.process_commands(message)
 
 
 @bot.event
 async def on_guild_join(guild):
     guild_id = guild.id
-    collection.insert_one({'_id': guild_id, 'prefix': ','})
-    print('done')
+    collection.insert_one({"_id": guild_id, "prefix": ","})
+    print("done")
 
 
 async def latency(ctx):
@@ -88,6 +89,7 @@ async def latency(ctx):
     time_2 = time.perf_counter()
     ping = round((time_2 - time_1) * 1000)
     await ctx.send(f"ping = {ping}")
+
 
 try:
     bot.run(DISCORD_TOKEN)
