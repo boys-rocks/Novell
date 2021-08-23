2bbf10b30c0226229efcbd86512d0c9bc5c249cb
Thomas Staats <thomas.staats443@gmail.com>
2021-08-22 22:51:43 -0700

    Delete helpers directory

--- helpers/checkParams.py
+++
@@ -1,9 +0,0 @@
-def checkCommandParams(bot, args):
-    values = []
-    for each in bot.get_command(args).params.keys():
-        if each != "ctx" and each != "self":
-            values.append(each)
-    if values:
-        return values
-    else:
-        return None
