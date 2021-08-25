a1a4d187b9e5e51b9b72033a58049884e90df9c2
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2021-08-25 11:14:01 +0530

    Create genav.py

---
+++ cogs/games/genav.py
@@ -0,0 +1,17 @@
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+#Random av command
+@bot.command()
+async def genav(ctx, seed=None):
+  if seed is None:
+    seed = random.sample(string.ascii_letters, 5)
+  async with aiohttp.ClientSession() as session:
+    async with session.get(f"https://avatars.dicebear.com/api/micah/{seed}.svg?mood[]=happy") as resp:
+      img_data = await resp.content.read()
+  with open('img.svg', 'wb') as handler:
+    handler.write(img_data)
+  drawing = svg2rlg('img.svg')
+  renderPM.drawToFile(drawing, 'img2.png', fmt='PNG')
+  with open("img2.png", "rb") as fh:
+    f = discord.File(fh, filename="av.png")
+  await ctx.reply(file=f)
