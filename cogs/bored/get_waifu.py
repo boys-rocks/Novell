131a682b262e1b4c9759b196cc2b95846c9c4f80
Baneet Parmar <ign.itami@gmail.com>
2021-09-07 02:02:41 +0530

    Update get_waifu.py

--- cogs/bored/get_waifu.py
+++ cogs/bored/get_waifu.py
@@ -10,15 +10,12 @@ class GetWaifu(commands.Cog):
     @commands.command(name="waifu", help="Get yourself a Waifu.")
     async def get_waifu(self, ctx, query="waifu"):
         with requests.get(url=f"https://api.waifu.pics/sfw/{query}") as response:
-            if response.status_code == 200:
-                try:
-                    await ctx.reply(response.json()["url"])
-                except:
-                    await ctx.reply(
-                        f"{query} don't wanna be your waifu. Try something else."
-                    )
-            else:
-                await ctx.send("API is down")
+            try:
+                await ctx.reply(response.json()["url"])
+            except:
+                await ctx.reply(
+                    f"{query} don't wanna be your waifu. Try something else."
+                )
 
 
 def setup(bot):
