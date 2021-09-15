20772633f521ded74eaffd141013cc91bb147eda
Baneet Parmar <ign.itami@gmail.com>
2021-09-16 02:31:04 +0530

    add slow_mode

--- cogs/admin/moderation.py
+++ cogs/admin/moderation.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 
 class Moderation(commands.Cog):
+    slow_mode_status = False
     """
     Moderation Module
     """
@@ -61,7 +62,7 @@ class Moderation(commands.Cog):
     @commands.command(
         help="Purges Messages",
         aliases=[
-            "tensei-shinra",
+            "allmightypush",
         ],
     )
     @commands.has_permissions(manage_messages=True)
@@ -79,6 +80,56 @@ class Moderation(commands.Cog):
             embed=discord.Embed(title="Purged: ", description=f"{limit} messages."),
             delete_after=5,
         )
+
+    @commands.command(name="slowmode", help="sets delay for messages")
+    @commands.has_permissions(manage_messages=True)
+    async def slow_mode(self, ctx, delay: str = None):
+        """
+        set delay in a channel
+
+
+        :param delay: delay time, defaults to None
+        :type delay: str, optional
+        """
+        if delay is None and self.slow_mode_status is False:
+            self.slow_mode_status = True
+            await ctx.send("Slow Mode Activated with `1 minute` delay.")
+            await ctx.channel.edit(slowmode_delay=60)
+        elif delay is None and self.slow_mode_status is True:
+            self.slow_mode_status = False
+            await ctx.send("Slow Mode Deactivated.")
+            await ctx.channel.edit(slowmode_delay=0)
+        else:
+            try:
+                self.slow_mode_status = True
+                delay_mod = delay.lower()
+                if (
+                    len(delay[: delay_mod.find("h")]) > 1
+                    or len(delay[delay_mod.find("h") : delay_mod.find("m")]) > 2
+                    or len(delay_mod[delay_mod.find("m") : -1]) > 2
+                ):
+                    await ctx.send("Invalid delay time `expected format:0h00m00s`")
+                    return
+                if "s" in delay_mod or "h" in delay_mod or "m" in delay_mod:
+                    delay_mod = delay_mod.replace("s", "")
+                    delay_mod = delay_mod.replace("m", "*60+")
+                    delay_mod = delay_mod.replace("h", "*3600+")
+                    if delay_mod.endswith("+"):
+                        delay_mod = delay_mod[:-1]
+                    total_delay = eval(delay_mod)
+                    if total_delay > 21600:
+                        await ctx.send("Invalid Delay (max limit is 6hrs)")
+                        return
+                    else:
+                        await ctx.send(f"Slow Mode activated with `{delay}` delay.")
+                        await ctx.channel.edit(slowmode_delay=total_delay)
+                else:
+                    await ctx.send(f"Slow Mode activated with `{delay} seconds` delay.")
+                    await ctx.channel.edit(slowmode_delay=int(delay))
+
+            except Exception as error:
+                await ctx.send(f"Invalid Delay{error}")
+                print(error)
 
 
 def setup(bot):
